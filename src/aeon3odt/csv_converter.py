77d0af9a848963548a8095760dfe80cc8b44a544
peter88213 <peter.88213@t-online.de>
2021-10-29 11:46:33 +0200

    Update to paeon v0.8.0

--- src/aeon3odt/csv_converter.py
+++ src/aeon3odt/csv_converter.py
@@ -5,7 +5,7 @@ For further information see https://github.com/peter88213/aeon3odt
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
 """
 from pywriter.converter.yw_cnv_ui import YwCnvUi
-from paeon.csv_timeline import CsvTimeline
+from paeon.csv_timeline3 import CsvTimeline3
 
 from aeon3odt.odt_full_synopsis import OdtFullSynopsis
 from aeon3odt.odt_brief_synopsis import OdtBriefSynopsis
@@ -16,7 +16,7 @@ from aeon3odt.odt_report import OdtReport
 
 
 class CsvConverter(YwCnvUi):
-    EXPORT_SOURCE_CLASSES = [CsvTimeline]
+    EXPORT_SOURCE_CLASSES = [CsvTimeline3]
     EXPORT_TARGET_CLASSES = [OdtFullSynopsis,
                              OdtBriefSynopsis,
                              OdtChapterOverview,
