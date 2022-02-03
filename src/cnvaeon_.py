aa1a2f3f2c06612bfa01284373f58dbf337b9a85
peter88213 <peter.88213@t-online.de>
2022-02-03 16:47:40 +0100

    Make the requirement for Python 3.6+.

--- src/cnvaeon_.py
+++ src/cnvaeon_.py
@@ -1,7 +1,7 @@
 """Convert Aeon Timeline project data to odt. 
 
 Version @release
-
+Requires Python 3.6+
 Copyright (c) 2021 Peter Triesberger
 For further information see https://github.com/peter88213/aeon3odt
 Published under the MIT License (https://opensource.org/licenses/mit-license.php)
